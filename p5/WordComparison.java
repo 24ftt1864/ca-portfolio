fb4b21bacaa5f58f01da87d736bb826ef4d888ef
24ftt1864 <24FTT1864@student.pb.edu.bn>
2025-08-28 18:57:49 +0800

    Add files via upload

--- p5/WordComparison.java
+++ p5/WordComparison.java
@@ -8,7 +8,7 @@ public class WordComparison {
         System.out.print("Enter the first word: ");
         String word1=input.nextLine();
 
-        System.out.print("Enter the first word: ");
+        System.out.print("Enter the second word: ");
         String word2=input.nextLine();
 
         if (word1.equalsIgnoreCase(word2)){
